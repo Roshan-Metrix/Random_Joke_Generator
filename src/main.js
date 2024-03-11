fc17788ea5f500bcb7fd12540b433218c79b66c8
Roshan Patel <roshanpatel12309@gmail.com>
2024-03-12 04:30:16 +0545

    Dark Light mode added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+const darkLight = document.getElementById('modeSwitch');
+const body = document.querySelector('body');
 
+darkLight.addEventListener('click',()=>{
+  body.classList.toggle('dark');
+  body.classList.toggle('bg-black');
+});
